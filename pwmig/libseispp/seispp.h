c2ccc5b9af6220594137dc6802eee3ab1a066c29
pavlis <pavlis>
2003-03-01 14:55:43 +0000

    Minor changes to class definitions.  Undoubtedly not the last changes.

--- pwmig/libseispp/seispp.h
+++ pwmig/libseispp/seispp.h
@@ -18,13 +18,13 @@ public:
 	~Time_Series() {  if(s!=NULL) delete s; };
 };
 
-class Three_Component_Seismogram : public Time_Series
+class Three_Component_Seismogram
 {
 public:
 	Metadata md;
 	//Using a Time_Series object adds overhead and memory use 
 	//by duplicating metadata, but adds generality
-	Time_Series x1, x2, x3;
+	Time_Series x[3];
 	Three_Component_Seismogram();
 	Three_Component_Seismogram(int nsamp);
 	~Three_Component_Seismogram();
@@ -33,10 +33,10 @@ class Time_Series_Ensemble
 {
 public:  
 	Metadata md;
-	vector <Time_Series> ts;
+	vector <Time_Series *> tse;
 
 	Time_Series_Ensemble();
-	Time_Series_Ensemble(int nts);
+	Time_Series_Ensemble(int nts, int nsamp);
 	Time_Series_Ensemble(Pf_ensemble *pfe);
 	// This function is bad form, but I see no alernative
 	// It's purpose is to initialize the namespace for the 
@@ -53,10 +53,10 @@ class Three_Component_Ensemble
 {
 public:
 	Metadata md;
-	vector <Three_Component_Seismogram> tcs;
+	vector <Three_Component_Seismogram> tcse;
 
 	Three_Component_Ensemble();
-	Three_Component_Ensemble(int nts);
+	Three_Component_Ensemble(int nsta, int nsamp);
 	Three_Component_Ensemble(Pf_ensemble *pfe);
 	void add_to_ensemble_mdlist(char *s){pushtbl(mdlist,s);};
 private:
