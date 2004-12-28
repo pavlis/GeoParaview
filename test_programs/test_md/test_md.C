98d752666e5b15c814874e6136cf9f09a2f38eaf
pavlis <pavlis>
2004-12-28 21:51:41 +0000

    Revisions for updates in metadata object definition adding new tests.

--- test_programs/test_md/test_md.C
+++ test_programs/test_md/test_md.C
@@ -7,26 +7,56 @@ using namespace SEISPP;
 // test routine for metadata functions
 int main(int argc, char **argv)
 {
-	string pfname="test_md";;
-	string amname="Attribute_Map";
+	//string pfname("test_md");
+	char *pfname="test_md";
+	string amname("Attribute_Map");
 	int ierr;
 
 	Pf *pf;
 	
 	//if(pfread(const_cast<char *>(pfname.c_str),&pf))
-	if(pfread("test_md",&pf))
+	if(pfread(pfname,&pf))
 	{
 		cerr << "Pfread error"<<endl;
 		exit(-1);
 	}
 	try {
-		cout << "Trying Attribute_map pf constructor"<<endl;
-		SEISPP::Attribute_map am(pf,amname);
+		cout << "Trying Attribute_Map default constructor"<<endl;
+		Attribute_Map amd;
+		cout << "Default map has "<< amd.attributes.size()
+				<< "members" << endl;
+		cout << "Trying Attribute_Map pf constructor"<<endl;
+		SEISPP::Attribute_Map am(pf,amname);
 		cout << "Trying copy constructor and = operator"<<endl;
-		SEISPP::Attribute_map am2(am);
+		SEISPP::Attribute_Map am2(am);
 		am=am2;
 		cout << "Trying pfget_mdlist function with tag mdlist" << endl;
 		Metadata_list mdl=pfget_mdlist(pf,"mdlist");
+		cout << "Trying to copy mdlist" << endl;
+		Metadata_list mdl2;
+		mdl2=mdl;
+		cout << "Original and copy of mdlist read" << endl;
+		Metadata_list::iterator mdi,mdi2;
+		for(mdi=mdl.begin(),mdi2=mdl2.begin();mdi!=mdl.end();++mdi,++mdi2)
+		{
+			cout << mdi->tag 
+			<< "," 
+			<< mdi2->tag 
+			<< "," 
+			<<mdi->mdt
+			<< "," 
+			<<mdi2->mdt<<endl;
+		}
+		cout << "Testing delete hypothesis on list object " << endl;
+		Metadata_list *mdl3=new Metadata_list(mdl);
+		delete mdl3;
+		for(mdi=mdl.begin();mdi!=mdl.end();++mdi)
+		{
+			cout << mdi->tag 
+			<< "," 
+			<<mdi->mdt<<endl;
+		}
+
 		cout << "Trying to build Metadata objects" << endl;
 		cout << "Trying default constructor" << endl;
 		Metadata mdplain;
@@ -39,15 +69,27 @@ int main(int argc, char **argv)
 		Dbptr db;
 		char *dbname="testdb";
 		ierr=dbopen(dbname,"r+",&db);
-		db=dblookup(db,0,"wfdisc",0,0);
-		db.record = 0;
-		Metadata *mddb = new Metadata(db,mdl,am2);
+		//db=dblookup(db,0,"wfdisc",0,0);
+		Datascope_Handle dbh(db,pf,string("dbprocess_list"));
+		dbh.rewind();
+		Metadata *mddb = new Metadata(dynamic_cast<Database_Handle&>(dbh),
+					mdl,am2);
 		cout << *mddb;
+		cout << "Trying nested pf constructor" << endl;
+		Metadata mdnest(pf,string("test_nested_tag"));
+		cout<<"test_double="<<mdnest.get_double("test_double")<<endl;
+		cout<<"test_int="<<mdnest.get_int("test_int")<<endl;
+		cout<<"test_string="<<mdnest.get_string("test_string")<<endl;
+
 	}
 	catch (Metadata_error mess)
 	{
 		mess.log_error();
 		exit(-1);
+	}
+	catch (seispp_error sess)
+	{
+		sess.log_error();
 	}
 	
 }
