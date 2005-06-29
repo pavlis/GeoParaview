e2715b8267ab693339fd4b3c4ced52fa2be30f4e
pavlis <pavlis>
2005-06-29 12:52:59 +0000

    Added dbsave for final test runs.

--- test_programs/test_newreaders/test_newreaders.cc
+++ test_programs/test_newreaders/test_newreaders.cc
@@ -7,14 +7,15 @@ using namespace SEISPP;
 void usage(char *prog)
 {
 	cerr << prog << ":  "
-		<< "db < list_of_time_intervals_file"<<endl;
+		<< "dbin dbout < list_of_time_intervals_file"<<endl;
 	exit(-1);
 }
 int main(int argc, char **argv)
 {
 	TimeSeriesEnsemble *tse;
-	if(argc!=2) usage(argv[0]);
+	if(argc!=3) usage(argv[0]);
 	string dbname(argv[1]);
+	string dboutname(argv[2]);
 	ios::sync_with_stdio();
 	char tstart[80],tend[80];
 	Pf *pf;  
@@ -25,7 +26,10 @@ int main(int argc, char **argv)
 	}
 
 	try {
-		DatascopeHandle dbh(dbname,false); // open read only
+		AttributeMap am("css3.0");
+		MetadataList mdout=pfget_mdlist(pf,"savelist");
+		DatascopeHandle dbh(dbname,true); // open read only
+		DatascopeHandle dbhout(dboutname,false);
 		TimeWindow twin;
 		while(fscanf(stdin,"%s%s",tstart,tend)!=EOF)
 		{
@@ -40,10 +44,13 @@ int main(int argc, char **argv)
 				twin);
 			for(i=0;i<tse->member.size();++i)
 			{
-				cout << "sta="<<tse->member[i].get_string("sta")<<endl;
-				cout << "chan="<<tse->member[i].get_string("chan")<<endl;
-				cout << "time="<<tse->member[i].get_double("time")<<endl;
-				cout << endl;
+				cout << "Saving sta="<<tse->member[i].get_string("sta")<<endl
+					<< "chan="<<tse->member[i].get_string("chan")<<endl
+					<< "time="<<tse->member[i].get_double("time")<<endl;
+				tse->member[i].put("dir","wf");
+				tse->member[i].put("dfile","testout.w");
+				dbsave(tse->member[i],dbhout.db,"wfdisc",
+					mdout,am);
 			}
 			delete tse;
 
