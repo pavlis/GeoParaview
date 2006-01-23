0a0efc866ed4d655c04df83ce15fd5c6392417a3
pavlis <pavlis>
2006-01-23 22:42:54 +0000

    Fix to be consistent with library changes in libseispp.

--- test_programs/test_slow/test_slow.cc
+++ test_programs/test_slow/test_slow.cc
@@ -1,18 +1,19 @@
+#include <iostream>
 #include "coords.h"
 #include "stock.h"
 #include "pf.h"
-#include "seispp.h"
+#include "slowness.h"
 using namespace SEISPP;
 int main(int argc, char **argv)
 {
 	Pf *pf;
 	pfread("test_slow",&pf);
 	string tag("Slowness_Grid_Example");
-	Rectangular_Slowness_Grid g(pf,tag);
+	RectangularSlownessGrid g(pf,tag);
 	for(int i=0;i<g.nux;++i)
 		for(int j=0;j<g.nuy;++j)
 		{
-			Slowness_vector u=g.slow(i,j);
+			SlownessVector u=g.slow(i,j);
 			cout << g.ux(i) << " "
 				<< u.ux << " "
 				<< g.uy(j) << " "
