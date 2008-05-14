6994dbc9c4c5ec7d9cf9dd951ea1b73f07b41209
pavlis <pavlis>
2008-05-14 15:27:34 +0000

    Not a working version.  Punt this to quarry so I can use totalview to sort out some mysterious problems.

--- pwmig/convertSacRTdata/convertRTdata.cc
+++ pwmig/convertSacRTdata/convertRTdata.cc
@@ -2,6 +2,7 @@
 for pwmig.  That means 3c data objects.  See design file in this directory
 for more info. */
 #include <math.h>
+#include <sstream>
 #include "coords.h"
 #include "stock.h"
 #include "seispp.h"
@@ -51,12 +52,41 @@ int main(int argc, char **argv)
 	try {
 		AttributeMap am("css3.0");
 		DatascopeHandle dbhi(dbin,true);
+		/* This set of db manipulations is a bit odd, 
+		but seems to work with these data. */
+		dbhi.lookup("wfdisc");
+		dbhi.natural_join("arrival");
+		string sstr("iphase=~/P/");
+		dbhi.subset(sstr);
+		dbhi.natural_join("site");
+		list<string> jk;
+		jk.push_back("arid");
+		dbhi.leftjoin("assoc",jk,jk);
+		jk.clear();
+		jk.push_back("orid");
+		dbhi.leftjoin("origin",jk,jk);
+		jk.clear();
+		jk.push_back("evid");
+		dbhi.leftjoin("event",jk,jk);
+		sstr="orid==prefor";
+		dbhi.subset(sstr);
+
+		/* Old code - did not work for mysterious reasons 
 		dbhi.lookup("event");
 		dbhi.natural_join("origin");
 		dbhi.natural_join("assoc");
 		dbhi.natural_join("arrival");
-		dbhi.natural_join("wfdisc");
+		string sstr("iphase=~/P/");
+		dbhi.subset(sstr);
+		list<string> jk1,jk2;
+		jk1.push_back(string("arrival.time"));
+		jk2.push_back(string("wfdisc.time::wfdisc.endtime"));
+		dbhi.join("wfdisc",jk1,jk2);
 		dbhi.natural_join("site");
+		**********************************************/
+
+		cout << "Working input view has "<<dbhi.number_tuples()
+			<<" rows"<<endl;
 		DatascopeHandle dbho(dbout,false);
 		dbho.lookup("wfprocess");
 		/* Need to put stuff in evlink and sclink for pwmig */
@@ -68,9 +98,10 @@ int main(int argc, char **argv)
 		int j,k;
 		double slat,slon,sdepth,stime;
 		string method("tttaup"),model("iasp91");
-		double stalat,stalon;
+		double stalat,stalon,staelev;
 		double seaz,az;
 		double a,b;
+		double atime;
 		int evid,pwfid;
 		string sta;
 		/* frozen for now */
@@ -86,23 +117,45 @@ int main(int argc, char **argv)
 			slon=d.get_double("source_lon");
 			evid=d.get_int("evid");
 			cout << "Processing event "<<evid
-				<< "located at (lat,lon)=("<<slat<<", "<<slon<<") ";
+				<< " located at (lat,lon)=("<<slat
+				<<", "<<slon<<") "<<endl;
+
 			slat=rad(slat);
 			slon=rad(slon);
 			sdepth=d.get_double("source_depth");
 			stime=d.get_double("source_time");
 			Hypocenter h(slat,slon,sdepth,stime,method,model);
 			sta=d.get_string("sta");
-			cout <<" station="<<sta;
 			stalat=d.get_double("sta_lat");
 			stalon=d.get_double("sta_lon");
+			staelev=d.get_double("sta_elev");
 			stalat=rad(stalat);
 			stalon=rad(stalon);
+			/* We have to fudge the start time because
+			Fenglin's rf code seems to not keep absolute
+			time.  Well, it might be in sac2db, but in
+			any case the arrival.time of P is the same
+			as the origin time.  Thus, to fix this start 
+			time we find the time difference between atime
+			and start time of the trace, compute P travel time,
+			and then correct t0 to make P time match theoretical
+			time exactly. */
+			atime=d.get_double("atime");
+			double ptime=h.ptime(stalat,stalon,staelev);	
+			double oldt0=d.t0;
+			double pdt=atime-oldt0;
+			cout << sta << " original atime - t0 ="<<pdt<<endl;
+			d.t0=ptime-pdt;
+			cout << "t0 of this seismogram changed by "
+				<< d.t0-oldt0 
+				<< " seconds"<<endl;
+			
 			seaz=h.seaz(stalat,stalon);
 			/* radial is defined as propagation direction, but seaz is back azimuth */
 			az=seaz+M_PI;
 			if(az>(2*M_PI)) az-=M_PI;
-			cout <<" Computed radial azimuth"<<deg(az)<<endl;
+			cout <<" station="<<sta
+				<<" computed radial azimuth="<<deg(az)<<endl;
 			/* Now we have to set the transformation matrix from az.  Convention here
 			is we put R into x2, so the rotation angle turns out to be -az when you 
 			work through the difference between rotation from the x1 axis and an
