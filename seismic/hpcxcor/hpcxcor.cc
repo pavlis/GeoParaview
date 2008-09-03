471e4386338df194606b95e9eadb0c5bc7109436
pavlis <pavlis>
2008-09-03 20:56:59 +0000

    Partial modification.  Check in to get a simple backup.

--- seismic/hpcxcor/hpcxcor.cc
+++ seismic/hpcxcor/hpcxcor.cc
@@ -1,5 +1,8 @@
 #include "seispp.h"
 #include "XcorProcessingEngine.h"
+#ifdef MPI_SET
+        #include <mpi.h>
+#endif
 using namespace std;
 using namespace SEISPP;
 /* This program is a variant of dbxcor and rtxcor.  It is aimed as a batch
@@ -19,6 +22,12 @@ issue that will evolve.  I'm writing this right now at the design
 phase to collect my ideas.
 */
 
+/* List of methods used to select one or more traces as reference event
+for MultichannelCorrelator */
+enum ReferenceTraceMethod {UseAll, MaxSnrDB, MaxSnrComputed, MaxPeakAmplitude};
+/* List of options for deciding with ensemble is "best" if run in a
+mode with multiple reference traces */
+enum BestEnsembleMethod {MaxSumWeights, MaxSurvivors};
 
 
 /* Finds the reference trace as the index position by a
@@ -79,7 +88,7 @@ stderr if there are any issues.
 
 md is a Metadata representation of a control parameter file */
 	
-bool validate_parameters(Metadata md,XcorEngineMode mode)
+bool validate_parameters(Metadata md,XcorEngineMode mode,string qfile, string origindb)
 {
 	bool result(true);
 	switch(mode)
@@ -89,6 +98,12 @@ bool validate_parameters(Metadata md,XcorEngineMode mode)
 	case GenericGathers:
 		break;
 	case ContinuousDB:
+		if(origindb.size()<=0 || qfile.size()<=0)
+			cerr << "Usage error.  "
+				<< "The -q and -e arguments are required "
+				<< "when running in ContinuousDB processing mode"<<endl;
+			result=false;
+			
 	default:
 	};
 	/* dummy for now, always true */
@@ -124,7 +139,7 @@ XcorEngineMode get_engine_mode(Metadata& md)
 
 void usage()
 {
-	cerr << "hpcxcor db [-o dbout -q queuefile -pf pffile]"<<endl;
+	cerr << "hpcxcor db [-o dbout -e origindb -q queuefile -pf pffile]"<<endl;
 	exit(-1);
 }
 bool SEISPP::SEISPP_verbose(false);
@@ -132,10 +147,28 @@ int main(int argc, char **argv)
 {
 	int i;
 	if(argc<2) usage();
+        elog_init(argc,argv);
+#ifdef MPI_SET
+
+/*
+        Initialize the MPI parallel environment, MPI_Init
+        must be called before any other MPI call.
+        rank -- rank of a specific process
+        mp -- the total number of processes. This whole
+                process group is called MPI_COMM_WORLD
+*/
+        int rank, np;
+
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &np);
+#endif
+
 	string pfname("hpcxcor");
 	string dbname(argv[1]);
 	string dbout(dbname);  // same as input by default
 	string queuefile("");
+	string origindb("");
 	for(i=2;i<argc;++i)
 	{
 		string argtest(argv[i]);
@@ -156,6 +189,12 @@ int main(int argc, char **argv)
 			++i
 			if(i>=argc)usage();
 			dbout=string(argv[i]);
+		}
+		if(argtest=="-e")
+		{
+			++i
+			if(i>=argc)usage();
+			origindb=string(argv[i]);
 		}
 		else
 			usage();
@@ -195,7 +234,7 @@ int main(int argc, char **argv)
 		/* This is here to do some sanity checks on pf an exit if
 		there are inconsisties.   This function should return a false
 		if parameters are inconsistent.*/
-		if(validate_parameters(global_md,enginemode))
+		if(validate_parameters(global_md,enginemode,queuefile,origindb))
 		{
 			cerr << "hcpxcor:  parameter inconsistency.  Cannot continue."
 				<< "Fix errors noted above and try again"<<endl;
@@ -210,9 +249,127 @@ int main(int argc, char **argv)
 				<< endl <<"Cannot continue.  Exiting. "<<endl;
 			exit(-2);
 		}
+		/* The engine contains an image of the waveform db view.  We need it.*/
+		DatabaseHandle *dbhwf = xpe.get_db(dbname);
 		Hypocenter h;  /* Only used in ContinousDB mode, but have to declare now */
+		/* In continuous mode we need to loop through the event database.  We
+		do this by building a handle to origindb and a linked queue */
+		DatascopeHandle dbhorigin;
+		if(enginemode==ContinuousDB)
+			dbhorigin=DatascopeHandle(origindb,true);
+			dbhorigin.lookup("event");
+			dbhorigin.natural_join("origin");
+			dbhorigin.subset("orid==prefor");
+			if(dbhorigin.number_tuples()<=0)
+			{
+				cerr << "event->origin prefor subset is empty.  Check database "
+					<< origindb<<endl;
+				exit(-1);
+			}
+			dbhorigin.rewind();
+			dpq=DatascopeProcessingQueue(dbhorigin,queuefile);
+		}
 
+		/* Top of loop over gathers.   Algorithm loops through each gather.
+		Eventually plan an mpi split here.  i.e. this loop is a perfect
+		data-driven, embarassingly parallel process. &/
+		int gathernumber(0);
+		bool keeplooping(true);
+		/* We push sorted copies of each ensemble to this vector of ensembles.
+		This could get out of control for large ensembles, but we'll try it 
+		until it proves problematic. */
+		vector<TimeSeriesEnsemble> trials;
+		int load_status;  // Return code from load_data used to test for end of data 
+		do {
+#ifdef MPI_SET
+		    if(gathernumber%np == rank)
+		    {
+#endif
+			
+			/* load data.  Unfortunately, different modes do this differently */
+			switch(enginemode)
+			{
+			case EventGathers:
+			case GenericGathers:
+				/* this method is odd because it marks the outcome of 
+				the last gather processed.  Works correctly for first pass
+				doing nothing to the queue. */
+				load_status=xpe.load_data(*dbhwf,FINISHED);
+				break;
+			case ContinuousDB:
+				dpq.set_to_current(dbhorigin);
+				h=load_hypo(dbhorigin);
+				xpe.load_data(h);
+			};
+			TimeSeriesEnsemble *tse=xpe.get_waveforms_gui();
+			/* We have to deal with the critical issue of setting
+			the event to be used as the reference trace for the MultichannelCorrelator.
+			We handle that here through an enum with different methods called.
+			This will make it easier to expand new options that might surface.
+			Each procedure returns a list of ensemble members to be used in 
+			a succession of trials.  Note for maintenance:  be careful xpe
+			does not unintentionally sort tse since we are given raw pointer
+			to the original data that will be sorted during analysis, for example. */
+			list<int> reference_traces;
+			list<int>::iterator rtptr;
+enum ReferenceTraceMethod {UseAll, MaxSnrDB, MaxSnrComputed, MaxPeakAmplitude};
+			switch(ReferenceTraceMethod)
+			{
+			case UseAll:
+			case MaxSnrDB:
+			case MaxSnrComputed:
+			case MaxPeakAmplitude:
+			};
+			trials.clear();
+			for(rtptr=reference_traces.begin(),i=0;
+				rtptr!=reference_traces.end();
+						++rtptr,++i)
+			{
+				asetting.set_ref_trace(*rtptr);
+				/* necessary because analysis call nearly always scrambles ensemble.
+				This is a bit inefficient, but hard to avoid without creating
+				havoc. */
+				if(i!=0) xpe.restore_original_ensemble();
+				xpe.change_analysis_setting(asetting);
+				xpe.analyze();
+				/* Must refresh this pointer since restore_original_ensemble is
+				guaranteed to change it */
+				tse=xpe.get_waveforms_gui();
+				trials.push_back(*tse);
+			}
+			/*Select the version considered best and save */
+			vector<TimeSeriesEnsemble>::iterator bestens;
+			/* Passing this the global_md is a backdoor way to get 
+			any parameters needed by any of the allowed methods. 
+			Not best practice, but preferable to modifying the engine. */
+			bestens=find_best_ensemble(trials, BestEnsembleMethod,
+				global_md);
+			/* We cannot use the save_results methods in the engine 
+			because it would be very evil to delete and modify the
+			tse pointer.  Hence we largely duplicate code from XcorProcessingEngine
+			in the procedure called here.  see above*/
+			save_results(enginemode,xpe,*bestens);
+				
 
+#ifdef MPI_SET
+		    }
+#endif
+		    ++gathernumber;
+		    if(enginemode==ContinousDB) ++dbhorigin;
+		    /* Because of inconsistent input methods, we have this ugly construct to 
+		    break this loop */
+		    switch(enginemode):
+		    {
+		    case EventGathers:
+		    case GenericGathers:
+			if(load_status) keeplooping=false;
+			break;
+		    case ContinuousDB:
+			if(!dpq.has_data()) keeplooping=false;
+		    };
+			
+		}
+		while(keeplooping);
 		// The current method has a default filter applied
 		// immediately after reading.  Here that is the filter.
 		// To extend to multiple filters will require 
