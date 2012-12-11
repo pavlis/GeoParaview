b44b6c7f68cd79e16dbc06417c15f022ef0b9496
Gary Pavlis <pavlis@indiana.edu>
2012-12-11 07:28:18 -0500

    Clear debris of libseiswplot2 directory

--- lib/libseiswplot/Xevent_loop.c
+++
@@ -1,9 +0,0 @@
-#include <X11/Intrinsic.h>
-void *Xevent_Loop(void *arg);
-void *Xevent_loop(void *arg)
-{
-        XtAppContext *appc;
-        appc=(XtAppContext *)(arg);
-        XtAppMainLoop(*appc);
-        return(NULL);
-}
