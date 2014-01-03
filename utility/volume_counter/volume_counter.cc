00660fbaa1a2c089ed13cb7eb14b7ed3b15ed1bf
Gary Pavlis <pavlis@indiana.edu>
2014-01-03 14:00:10 -0500

    Add sum of area calculation - compiles but untested

--- utility/volume_counter/volume_counter.cc
+++ utility/volume_counter/volume_counter.cc
@@ -11,9 +11,11 @@ using namespace std;
 using namespace SEISPP;
 void usage()
 {
-    cerr << "volume_counter db out [-pf pffile -v] < infile"<<endl
+    cerr << "volume_counter db out [-ss subset_condition -pf pffile -v] < infile"<<endl
         << "   db is Antelope db  (needs event->origin and will write gcl tables"<<endl
         << "   out is name for output gclfield data (file name and in gclfield table)"<<endl
+        << "   (note out is vectorfield with 0 count and 1 area/volume ration estimate)" <<endl
+        << "   -ss defines subset condition applied before processing (can be important for efficiency)"<<endl
         << "   -pf for alternate pf (check pf before running of course)"<<endl
         << "   -v to turn on verbose error reporting"<<endl;;
     exit(-1);
@@ -51,6 +53,28 @@ class DistanceTest : public unary_function<Hypocenter,bool>
         GCLgrid3d *g;
 };
 
+double sum_areas(auto_ptr<EventCatalog> events,string magname,double nullvalue)
+{
+    try {
+        double sumresult(0.0);
+        events->rewind();
+        int nevents=events->size();
+        int i;
+        /* EventCatalog has an internal counter - odd increment operator 
+           is a syntatic oddity of usign an auto_ptr */
+        for(i=0;i<nevents;++i,++(*events))
+        {
+            Metadata aux=events->current_aux();
+            double mag=aux.get_double(magname);
+            /* assume anything negative is null */
+            if(mag<0.0) mag=nullvalue;
+            /* glp see momentscalingtheory.pdf for derivation of this formula*/
+            double area=pow(10.0,(mag-3.296));
+            sumresult += area;
+        }
+        return sumresult;
+    }catch(...){throw;};
+}
 bool SEISPP::SEISPP_verbose(false);
 int main(int argc, char **argv)
 {
@@ -58,6 +82,8 @@ int main(int argc, char **argv)
     string dbname(argv[1]);
     string outfield(argv[2]);
     string pffile("volume_counter");
+    bool apply_subset(false);
+    string subset_string;
     int i,j,k;
     for(i=3;i<argc;++i)
     {
@@ -68,6 +94,13 @@ int main(int argc, char **argv)
             if(i>=argc) usage();
             pffile=string(argv[i]);
         }
+        else if(sarg=="-ss")
+        {
+            ++i;
+            if(i>=argc) usage();
+            apply_subset=true;
+            subset_string=sarg;
+        }
         else if(sarg=="-v")
             SEISPP_verbose=true;
         else
@@ -77,9 +110,24 @@ int main(int argc, char **argv)
     ios::sync_with_stdio();
     try{
         DatascopeHandle dbh(dbname,false);
-        EventCatalog eqs(dbh);
+        if(apply_subset) 
+        {
+            if(SEISPP_verbose)
+                cout << "Original db size before subset application"
+                    << dbh.number_tuples()<<endl
+                    << "Applying this subset condition:  "
+                    << apply_subset<<endl;
+            dbh.subset(subset_string);
+            if(SEISPP_verbose)
+                cout << "Number of tuples after subset = "
+                    << dbh.number_tuples()<<endl;
+        }
         PfStyleMetadata control=SEISPP::pfread(pffile);
-        control.pfwrite(cout);
+        if(SEISPP_verbose)
+        {
+            cout << "Parameter file values read"<<endl;
+            control.pfwrite(cout);
+        }
         double search_radius=control.get_double("search_radius");   // this is in km
         string dir=control.get_string("output_directory");
         /* This is essential the same as makegclgrid but using the new PfStyleMetadata object */
@@ -102,11 +150,32 @@ int main(int argc, char **argv)
         i0=control.get_int("origin_offset_x1");
         j0=control.get_int("origin_offset_x2");
         string gridname=control.get_string("gridname");
+        bool use_netmag=control.get_bool("use_netmag");  
+        string magnitude_attribute=control.get_string("magnitude_attribute");
+        double null_magnitude_default=control.get_double("null_magnitude_default");
+        if(use_netmag)
+            dbh.natural_join(string("netmag"));
+        /* We always load ms, mb, and ml as they are part of origin*/
+        MetadataList maglist;
+        Metadata_typedef md;
+        md.tag=string("origin.ms");
+        md.mdt=MDreal;
+        maglist.push_back(md);
+        md.tag=string("origin.mb");
+        maglist.push_back(md);
+        md.tag=string("origin.ml");
+        if(use_netmag)
+        {
+            md.tag=string("netmag.magnitude");
+            maglist.push_back(md);
+        }
+        AttributeMap am("css3.0");
+        EventCatalog eqs(dbh,maglist,am);
         /* Use new here so we can save some memory once
            we create the field object */
         GCLgrid3d *g=new GCLgrid3d(n1,n2,n3,gridname,lat0,lon0,r0,
                 azimuth_y,dx1,dx2,dx3,i0,j0);
-        GCLscalarfield3d f(*g);
+        GCLvectorfield3d f(*g,2);
         /* this is not essential, but best to be explicit */
         f.zero();
         delete g;
@@ -125,8 +194,10 @@ int main(int argc, char **argv)
                     DistanceTest closeones(&f,i,j,k,search_radius);
                     auto_ptr<EventCatalog> locals=eqs.subset<DistanceTest>(closeones);
                     int count=locals->size();
+                    double asum=sum_areas(locals,magnitude_attribute,null_magnitude_default);
                     /* Note we convert to count per unit volume */
-                    f.val[i][j][k]=static_cast<double>(count)/volume;
+                    f.val[i][j][k][0]=static_cast<double>(count)/volume;
+                    f.val[i][j][k][1]=asum/volume;
                 }
             }
         /* This always writes the field output data to a file set by the name outfield */
